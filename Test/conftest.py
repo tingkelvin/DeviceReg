95f37b7fdebfc3e0cb9990903322b498ca27d798
Kelvin Ting <ting_kelvin@icloud.com>
2023-08-23 02:05:58 +0930

    Implemented sql query

--- Test/conftest.py
+++ Test/conftest.py
@@ -1,16 +1,37 @@
 import pytest
 import requests
 import json
+import pyodbc
 
 def pytest_addoption(parser):
     parser.addoption("--host", action="store", default="localhost:7071")
+    parser.addoption("--server", action="store")
+    parser.addoption("--username", action="store")
+    parser.addoption("--password", action="store")
+    parser.addoption("--database", action="store")
+    parser.addoption("--driver", action="store", default="{ODBC Driver 17 for SQL Server}")
 
 def pytest_generate_tests(metafunc):
     # This is called for every test. Only get/set command line arguments
     # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.host
-    if 'host' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("host", [option_value])
+    option_host = metafunc.config.option.host
+    option_server = metafunc.config.option.server
+    option_database = metafunc.config.option.database
+    option_username = metafunc.config.option.username
+    option_password = metafunc.config.option.password
+    option_driver = metafunc.config.option.driver
+    if 'host' in metafunc.fixturenames and option_host is not None:
+        metafunc.parametrize("host", [option_host])
+    if 'server' in metafunc.fixturenames and option_server is not None:
+        metafunc.parametrize("server", [option_server])
+    if 'database' in metafunc.fixturenames and option_database is not None:
+        metafunc.parametrize("database", [option_database])
+    if 'username' in metafunc.fixturenames and option_username is not None:
+        metafunc.parametrize("username", [option_username])
+    if 'password' in metafunc.fixturenames and option_password is not None:
+        metafunc.parametrize("password", [option_password])
+    if 'driver' in metafunc.fixturenames and option_driver is not None:
+        metafunc.parametrize("driver", [option_driver])
         
 class Utils:
     @staticmethod
@@ -20,6 +41,22 @@ class Utils:
         res = requests.post(url, headers=headers, json=data)
         print(f"{testname}-{testNo}: {res.status_code}")
         assert(res.status_code == 200)
+
+    def sql_query(query, server, database, username, password, driver):
+        ret = []
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                row = cursor.fetchone()
+                while row:
+                    ret.append(str(row[0]))
+                    row = cursor.fetchone()
+        return ret
+    
+    def clean_database(server, database, username, password, driver):
+        with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("DELETE FROM [dbo].[devices]")
         
 
 @pytest.fixture
--- Test/test_1.py
+++ Test/test_1.py
@@ -2,102 +2,106 @@
 from multiprocessing import Process
 import random
 import time
-import pyodbc
 
-server = 'deviceregappsql.database.windows.net'
-database = 'devicesdatabase'
-username = 'admin-sql'
-password = 'Abc!23321'   
-driver= '{ODBC Driver 17 for SQL Server}'
 
-# def test_one_10_get_request(host, utils, devicesData):
-#   print("Starting get request test")
-#   processPool = []
-#   # print(devicesData)
-#   data = {
-#     "devices": [
-#       devicesData["devices"][0]
-#     ]
-#   }
-#   for i in range(10):
-#     process = Process(target=utils.post_request, args=(host,data,"Get Request Test", i+1))
-#     processPool.append(process)
-#     process.start()
-#   for process in processPool:
-#     process.join()
-#   print("Get request test done")
+# server = 'deviceregappsql.database.windows.net'
+# database = 'devicesdatabase'
+# username = 'admin-sql'
+# password = 'Abc!23321'   
+# driver= '{ODBC Driver 17 for SQL Server}'
 
-# def test_two_10_post_request(host, utils, devicesData):
-#   print("Starting post request test")
-#   # print(devicesData)
-#   processPool = []
-#   data = {
-#     "correlationId": "84f84dc5-d0a7-440f-92e0-c926ad8aa709",
-#     "devices": devicesData["devices"][:3]
+def test_one_10_get_request(host, utils, devicesData):
+  print("Starting get request test")
+  processPool = []
+
+  data = {
+    "devices": [
+      devicesData["devices"][0]
+    ]
+  }
+
+  # generate 10 get request
+  for i in range(10):
+    process = Process(target=utils.post_request, args=(host,data,"Get Request Test", i+1))
+    processPool.append(process)
+    process.start()
+
+  # make sure all the process finish
+  for process in processPool:
+    process.join()
+  print("Get request test done")
+
+def test_two_10_post_request(host, utils, devicesData):
+  print("Starting post request test")
+  processPool = []
+  data = {
+    "correlationId": "84f84dc5-d0a7-440f-92e0-c926ad8aa709",
+    "devices": devicesData["devices"][:3]
     
-#   }
-#   # utils.post_request(host,data)
-#   # process = Process(target=utils.post_request, args=(host,data))
-#   for i in range(10):
-#     process = Process(target=utils.post_request, args=(host,data,"Post Request Test", i+i))
-#     processPool.append(process)
-#     process.start()
-#   for process in processPool:
-#     process.join()
-#   print("Post request test done")
+  }
 
-# def test_three_random_mix_request(host, utils, devicesData):
-#   print("Starting random mix request test")
-#   start = time.time()
-#   # print(devicesData)
-#   testDevices = []
-#   processPool = []
-#   max_number = 1000
-#   i = 0
-#   while i < max_number:
-#     choice = random.randint(0, 1)
-#     if choice == 1:
-#       data = {
-#         "devices": [
-#           devicesData["devices"][i]
-#         ]
-#       }
-#       testDevices += [devicesData["devices"][i]]
-#       process = Process(target=utils.post_request, args=(host,data,"Get Request Test", i+i))
-#       i += 1
-#     else:
-#       index =  min(i + random.randint(10, 50), max_number)
-#       data = {
-#       "devices": devicesData["devices"][i:index]   
-#       }
-#       process = Process(target=utils.post_request, args=(host,data,"Post Request Test", i+1))
-#       testDevices += devicesData["devices"][i:index]
-#       i = index
-#     processPool.append(process)
-#     process.start()
-#   for process in processPool:
-#     process.join()
+  # generate 10 post request
+  for i in range(10):
+    process = Process(target=utils.post_request, args=(host,data,"Post Request Test", i+i))
+    processPool.append(process)
+    process.start()
+  
+  # make sure all the process finish
+  for process in processPool:
+    process.join()
+  print("Post request test done")
 
-#   deviceNames = []
-#   for testDevice in testDevices:
-#     deviceNames.append(testDevice["id"])
+def test_three_random_mix_request(host, utils, devicesData, server, database, username, password, driver):
+  print("Starting random mix request test")
+  utils.clean_database(server, database, username, password, driver)
+  start = time.time()
+  testDevices = []
+  processPool = []
+  max_number = 1000
+  i = 0
 
-#   # assert all the test devices are tested
-#   assert(len(testDevices) == max_number and len(testDevices) == len(set(deviceNames)))
+  # generate 1000 random request 
+  while i < max_number:
+    choice = random.randint(0, 1)
+    if choice == 1:
+      data = {
+        "devices": [
+          devicesData["devices"][i]
+        ]
+      }
+      testDevices += [devicesData["devices"][i]]
+      process = Process(target=utils.post_request, args=(host,data,"Get Request Test", i+i))
+      i += 1
+    else:
+      index =  min(i + random.randint(10, 50), max_number)
+      data = {
+      "devices": devicesData["devices"][i:index]   
+      }
+      process = Process(target=utils.post_request, args=(host,data,"Post Request Test", i+1))
+      testDevices += devicesData["devices"][i:index]
+      i = index
+    processPool.append(process)
+    process.start()
+  for process in processPool:
+    process.join()
 
-#   # assert all request are done in 10 mins
-#   end = time.time()
-#   assert(end - start < 10*60)
-#   print("Random mix request test done")
+  # gather all the deviceId
+  deviceIds = []
+  for testDevice in testDevices:
+    deviceIds.append(testDevice["id"])
 
-def test_SQL_server():
-    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-      with conn.cursor() as cursor:
-          cursor.execute("SELECT * FROM [dbo].[devices]")
-          row = cursor.fetchone()
-          while row:
-              print(str(row[0]) + " " + str(row[1]))
-              row = cursor.fetchone()
+  # assert all the test devices are tested
+  assert(len(testDevices) == max_number and len(testDevices) == len(set(deviceIds)))
 
+  # assert all request are done in 10 mins
+  end = time.time()
+  assert(end - start < 10*60)
 
+  # assert all devices are written to sql database
+  queryDeviceIds = utils.sql_query("SELECT DeviceId FROM [dbo].[devices]", server, database, username, password, driver)
+  queryDeviceIds.sort()
+  deviceIds.sort()
+  assert(deviceIds == queryDeviceIds)
+
+  print("Random mix request test done")
 
