87027b4a731b77d5ff1b16cc57cb903641b74ff7
Kelvin Ting <ting_kelvin@icloud.com>
2023-08-22 22:36:55 +0930

    Added host argument

--- Test/test_1.py
+++ Test/test_1.py
@@ -7,27 +7,10 @@ import requests
 
 
 
-
-def post_request(host):
-  url = f"http://{host}/api/register"
-  headers = {"Content-Type": "application/json"}
-  data = {
-      "devices": [
-      {
-          "id":"DVID000001",
-          "Name": "device 1 Name",
-          "location":"Here",
-          "type": "1"
-      }
-      ]
-  }
-  res = requests.post(url, headers=headers, json=data)
-  assert(res.status_code == 200)
-
-def test_function_with_scenario_one(host):
+def test_function_with_scenario_one(host, utils):
   processPool = []
-  for i in range(10):
-    process = Process(target=post_request, args=(host,))
+  for i in range(40):
+    process = Process(target=utils.post_request, args=(host,))
     processPool.append(process)
     process.start()
   for process in processPool:
