e5e974ab4ac66effde41f094ea8e28f1902e6d45
tingk <kelvin.ting@codancomms.com>
2023-08-23 16:29:38 +0930

    Update ReadMe

--- utils/Client.cs
+++ utils/Client.cs
@@ -1,47 +1,50 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace AzureDevOps
 {
-    public class Client
+  public class Client
+  {
+    public static HttpClient GetHttpClient(string password)
     {
-      public static HttpClient GetHttpClient(string password)
+        // create http request client
+        HttpClient client = new HttpClient(new RetryHandler(new HttpClientHandler()));
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        client.DefaultRequestHeaders.Add("x-functions-key", password);
+        return client;
+    }
+  
+    static async Task<List<Asset>> ProcessDeviceAsync(Dictionary<string, Device> devicesHashMap)
+    {
+      if (devicesHashMap.Count == 1)
       {
-          // create http request client
-          HttpClient client = new HttpClient(new RetryHandler(new HttpClientHandler()));
-          client.DefaultRequestHeaders.Accept.Clear();
-          client.DefaultRequestHeaders.Accept.Add(
-              new MediaTypeWithQualityHeaderValue("application/json"));
-          client.DefaultRequestHeaders.Add("x-functions-key", password);
-          return client;
+          using HttpClient getClient = GetHttpClient(Environment.GetEnvironmentVariable("GET_API_KEY"));
+
+          // get the first element of the hashmap
+          var enumerator = devicesHashMap.GetEnumerator();
+          enumerator.MoveNext();
+          string anElement = enumerator.Current.Key;
+          
+          // create get request
+          string getRequestUrl = Environment.GetEnvironmentVariable("API_HOST") + anElement;
+          using HttpResponseMessage getResponse = await getClient.GetAsync(getRequestUrl);
+          Asset asset = JsonConvert.DeserializeObject<Asset>(getResponse.Content.ReadAsStringAsync().Result);
+          return new List<Asset>{asset};
       }
+
+      using HttpClient postClient = GetHttpClient(Environment.GetEnvironmentVariable("POST_API_KEY"));
+      string postRequestUrl = Environment.GetEnvironmentVariable("API_HOST");
+
+      // create post request
+      using HttpResponseMessage postResponse = await postClient.PostAsJsonAsync(
+          postRequestUrl, 
+          new jsonContent(deviceIds:devicesHashMap.Keys));
+      Assets assets = JsonConvert.DeserializeObject<Assets>(postResponse.Content.ReadAsStringAsync().Result);
+      return assets.devices;
     }
-        static async Task<List<Asset>> ProcessDeviceAsync(Dictionary<string, Device> devicesHashMap)
-    {
-        if (devicesHashMap.Count == 1)
-        {
-            using HttpClient getClient = GetHttpClient(Environment.GetEnvironmentVariable("GET_API_KEY"));
-
-            // get the first element of the hashmap
-            var enumerator = devicesHashMap.GetEnumerator();
-            enumerator.MoveNext();
-            string anElement = enumerator.Current.Key;
-            
-            // create get request
-            string getRequestUrl = Environment.GetEnvironmentVariable("API_HOST") + anElement;
-            using HttpResponseMessage getResponse = await getClient.GetAsync(getRequestUrl);
-            Asset asset = JsonConvert.DeserializeObject<Asset>(getResponse.Content.ReadAsStringAsync().Result);
-            return new List<Asset>{asset};
-        }
-
-        using HttpClient postClient = GetHttpClient(Environment.GetEnvironmentVariable("POST_API_KEY"));
-        string postRequestUrl = Environment.GetEnvironmentVariable("API_HOST");
-
-        // create post request
-        using HttpResponseMessage postResponse = await postClient.PostAsJsonAsync(
-            postRequestUrl, 
-            new jsonContent(deviceIds:devicesHashMap.Keys));
-        Assets assets = JsonConvert.DeserializeObject<Assets>(postResponse.Content.ReadAsStringAsync().Result);
-        return assets.devices;
-    }
+  }
 }
