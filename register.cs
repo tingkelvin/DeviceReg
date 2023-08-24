bdc23f2a116173b98924a7a1edd51d8f85c7066c
Kelvin Ting <ting_kelvin@icloud.com>
2023-08-24 22:21:03 +0930

    Error Handling

--- register.cs
+++ register.cs
@@ -20,47 +20,70 @@ namespace AzureDevOps
             ILogger log,
             [Sql(commandText: "dbo.Devices", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Device> deviceTable)
         {
-            // parsing request body
-            log.LogInformation("C# HTTP trigger processed a request.");
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            JArray devices = data?.devices;
+            try 
+            {
+                log.LogInformation("C# HTTP trigger processed a request.");
+                // parsing request body
         
-            Dictionary<string, Device> devicesHashMap = new Dictionary<string, Device>();
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+                JArray devices = data?.devices;
 
-            // build the hashmap to store the devices
-            foreach (JObject item in devices) 
+                if (devices.Count == 0)
+                {
+                    return new BadRequestObjectResult("There are no devices.");
+                }
+            
+                Dictionary<string, Device> devicesHashMap = new Dictionary<string, Device>();
+
+                // build the hashmap to store the devices
+                foreach (JObject item in devices) 
+                {
+                    string id = item.GetValue("id").ToString();
+                    string name = item.GetValue("Name").ToString();
+                    string type = item.GetValue("type").ToString();
+                    string location = item.GetValue("location").ToString();
+                    devicesHashMap.Add(id, new Device{
+                        DeviceId = id,
+                        Name = name,
+                        Type = type,
+                        Location = location
+                    });
+                }
+                // api call to retrive assetid
+                List<Asset> assets = await Client.ProcessDeviceAsync(devicesHashMap, 
+                                                                    Environment.GetEnvironmentVariable("GET_API_KEY"), 
+                                                                    Environment.GetEnvironmentVariable("POST_API_KEY"),
+                                                                    Environment.GetEnvironmentVariable("API_HOST"));
+
+                // write the asset id to the hashmap
+                foreach(Asset asset in assets)
+                {
+                    devicesHashMap[asset.deviceId].AssetId = asset.assetId;
+                }
+
+                // write to sql database
+                foreach (var device in devicesHashMap){
+                    await deviceTable.AddAsync(device.Value);
+                }
+                await deviceTable.FlushAsync();
+                return new OkObjectResult("Register succesfully");
+            }
+            catch (JsonReaderException e)
             {
-                string id = item.GetValue("id").ToString();
-                string name = item.GetValue("Name").ToString();
-                string type = item.GetValue("type").ToString();
-                string location = item.GetValue("location").ToString();
-                devicesHashMap.Add(id, new Device{
-                    DeviceId = id,
-                    Name = name,
-                    Type = type,
-                    Location = location
-                });
+                log.LogInformation(e.ToString());
+                return new BadRequestObjectResult("Invalid body, cannot register");
             }
-
-            // api call to retrive assetid
-            List<Asset> assets = await Client.ProcessDeviceAsync(devicesHashMap, 
-                                                                Environment.GetEnvironmentVariable("GET_API_KEY"), 
-                                                                Environment.GetEnvironmentVariable("POST_API_KEY"),
-                                                                Environment.GetEnvironmentVariable("API_HOST"));
-
-            // write the asset id to the hashmap
-            foreach(Asset asset in assets)
+            catch (NullReferenceException e)
             {
-                devicesHashMap[asset.deviceId].AssetId = asset.assetId;
+                log.LogInformation(e.ToString());
+                return new BadRequestObjectResult("Invalid data, cannot register");
             }
-
-            // write to sql database
-            foreach (var device in devicesHashMap){
-                await deviceTable.AddAsync(device.Value);
+            catch (Exception e)
+            {
+                log.LogInformation(e.ToString());
+                return new BadRequestObjectResult("Error, cannot register");
             }
-            await deviceTable.FlushAsync();
-            return new OkObjectResult("Succesfully register.");
         }
     }
 }
